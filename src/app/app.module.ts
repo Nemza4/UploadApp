4fab095b05e99570089fc44e2b3ec4a641f0725c
nemza4 <nemavholags@gmail.com>
2018-09-27 11:03:31 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture';
+import { File } from '@ionic-native/file';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
+    
   ],
   imports: [
     BrowserModule,
@@ -18,13 +19,15 @@ import { HomePage } from '../pages/home/home';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
+  
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MediaCapture,
+    File
   ]
 })
 export class AppModule {}
